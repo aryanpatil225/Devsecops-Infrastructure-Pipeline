028f9947885a1b222ae541bdb89aa3aac5ac67a9
Aryan Patil <144831184+aryanpatil225@users.noreply.github.com>
2025-12-20 12:43:15 +0530

    test 1.1

--- terraform/main.tf
+++ terraform/main.tf
@@ -171,7 +171,7 @@ resource "aws_security_group" "web_sg" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = [var.admin_ssh_cidr]
+    cidr_blocks = ["0.0.0.0/0"]  # ❌ OPEN TO ENTIRE WORLD!
   }
 
   # Application port access
